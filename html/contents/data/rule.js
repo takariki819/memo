d50fac3e0874d326f76fcf34e7fa83a69afe6d00
takariki819 <takariki819@gmail.com>
2022-10-11 16:28:18 +0900

    10-11 gitignore編集

--- html/contents/data/rule.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function handle(event) {
-    event.preventDefault();
-}
-
-window.onload = function() {
-document.addEventListener('touchmove', handle, { passive: false });
-document.addEventListener('mousewheel', handle, { passive: false });
-}
